8b109f8130cf34d3fa5b71cc3db2de97d1350b08
uron <uron802@yahoo.co.jp>
2019-02-10 11:18:44 +0900

    php artisan migrate src

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // バージョン5.7.7より古いMySQLや、バージョン10.2.2より古いMariaDBを使用している場合に使用
+        Schema::defaultStringLength(191);
     }
 }
