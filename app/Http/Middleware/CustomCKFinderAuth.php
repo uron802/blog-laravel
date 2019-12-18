619ccda07c072dedc9a8fe3e0c42c0795a17b5f4
uron <uron802@yahoo.co.jp>
2019-12-18 14:12:56 +0000

    Apply fixes from StyleCI

--- app/Http/Middleware/CustomCKFinderAuth.php
+++ app/Http/Middleware/CustomCKFinderAuth.php
@@ -9,16 +9,18 @@ class CustomCKFinderAuth
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        config(['ckfinder.authentication' => function() {
+        config(['ckfinder.authentication' => function () {
             //@todo 要認証
             return true;
         }]);
+
         return $next($request);
     }
 }
