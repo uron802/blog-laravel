c1ed0bc4a75edc6bbfc2d2b624492bebcdfd7784
uron <38073941+uron802@users.noreply.github.com>
2022-09-06 14:53:13 +0900

    add certificate

--- config/database.php
+++ config/database.php
@@ -42,11 +42,12 @@ return [
 
         'mysql' => [
             'driver'         => 'mysql',
+            'url' => env('DATABASE_URL'),
             'host'           => env('DB_HOST', '127.0.0.1'),
             'port'           => env('DB_PORT', '3306'),
-            'database'       => env('DB_DATABASE', 'blog_laravel'),
-            'username'       => env('DB_USERNAME', 'blog_laravel'),
-            'password'       => env('DB_PASSWORD', 'blog_laravel'),
+            'database'       => env('DB_DATABASE', 'forge'),
+            'username'       => env('DB_USERNAME', 'forge'),
+            'password'       => env('DB_PASSWORD', ''),
             'unix_socket'    => env('DB_SOCKET', ''),
             'charset'        => 'utf8mb4',
             'collation'      => 'utf8mb4_unicode_ci',
@@ -54,6 +55,9 @@ return [
             'prefix_indexes' => true,
             'strict'         => true,
             'engine'         => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'pgsql' => [
